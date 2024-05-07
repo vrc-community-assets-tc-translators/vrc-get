2b93adc0783c61b269a68a8eed1a514834a0eb2d
夜嵐蝶Alma <97604181+yelandiealma@users.noreply.github.com>
2024-05-07 09:49:41 +0800

    Update i18n.ts for zh-hant

--- vrc-get-gui/lib/i18n.ts
+++ vrc-get-gui/lib/i18n.ts
@@ -4,6 +4,7 @@ import enJson from "@/locales/en.json5";
 import deJson from "@/locales/de.json5";
 import jaJson from "@/locales/ja.json5";
 import zh_cnJson from "@/locales/zh_cn.json5";
+import zh_hantJson from "@/locales/zh_hant.json5";
 import frJson from "@/locales/fr.json5";
 import {tc as tcOriginal} from "@/components/VGTrans";
 
@@ -13,6 +14,7 @@ const languageResources = {
 	ja: jaJson,
 	fr: frJson,
 	zh_cn: zh_cnJson,
+	zh_hant: zh_hantJson,
 }
 
 i18next
